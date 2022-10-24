6a5d802968fee9749657ebd90acdd8e71267c565
Oscar <yo@gmail.com>
2022-10-24 12:05:45 -0600

    Cambiando conexion a la base de datos

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,6 +1,6 @@
 <?php
 function conectarDB() : mysqli{ //host, usuario, clave, bd 
-    $db = new mysqli('localhost', 'root', 'root', 'bienesraices_crud'); //Conexion a base de datos
+    $db = new mysqli('localhost', 'id19747668_root', 'Oscar118817!', 'id19747668_bienesraices_crud'); //Conexion a base de datos
     if(!$db){
         echo "¡Error! No se pudo conectar";
         exit;
