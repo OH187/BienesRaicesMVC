68db34702c6b2a28a7b008b4e507475613a034ea
Oscar <yo@gmail.com>
2022-11-04 13:22:48 -0600

    Agregando htaccess

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,6 +1,8 @@
 <?php
 function conectarDB() : mysqli{ //host, usuario, clave, bd 
-    $db = new mysqli('localhost', 'id19747668_root', 'Oscar118817!', 'id19747668_bienesraices_crud'); //Conexion a base de datos
+    // $db = new mysqli('sql104.epizy.com', 'epiz_32914727', 'fgJtd8iXsdzYM', 'epiz_32914727_bienesraices');
+    // $db = new mysqli('localhost', 'id19747668_root', 'Oscar118817!', 'id19747668_bienesraices_crud');
+    $db = new mysqli('localhost', 'root', 'root', 'bienesraices_crud'); //Conexion a base de datos
     if(!$db){
         echo "¡Error! No se pudo conectar";
         exit;
